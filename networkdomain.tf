06bc9163647e43ec5407cc4edc8f7d5ad822906e
wninobla <william.ninobla@dimensiondata.com>
2016-11-28 13:22:15 -0800

    Update networkdomain.tf
    Added missing variable syntax

--- networkdomain.tf
+++ networkdomain.tf
@@ -2,8 +2,8 @@
 #  assets.  In this case, this resource creates network domains.
 
 resource "ddcloud_networkdomain" "networkdomain" {
-  name                 = "$var.networkdomain_name"
+  name                 = "${var.networkdomain_name}"
   description          = "New network domain created via Terraform and GoCD via Windows"
-  datacenter           = "$var.datacenter"
+  datacenter           = "${var.datacenter}"
   plan                 = "ADVANCED"
 }
