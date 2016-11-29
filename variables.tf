6c1eaa32580a525cc12332fb13c0065e66d79a3f
wninobla <wninobla@users.noreply.github.com>
2016-11-29 10:41:47 -0800

    Update variables.tf
    Added variable sections based on where the variables are used in the different .tf files that comprise the Terraform configuration files

--- variables.tf
+++ variables.tf
@@ -5,8 +5,16 @@
 #    - All variables MUST BE prefixed with TF_VAR_variable_name in GoCD AND
 #    - The prefix declaration is truncated in the variables.tf file or just variable_name
 
+#  Variables in provider.tf file
 variable "username" {} #Matches to TF_VAR_username
 variable "password" {} #Matches to TF_VAR_password
 variable "region" {} #Matches to TF_VAR_region
+
+#  Variables in network.tf file
 variable "datacenter" {} #Matches to TF_VAR_datacenter
 variable "networkdomain_name" {} #Matches to TF_VAR_networkdomain_name
+
+#  Variables in vlan.tf file
+variable "vlan_dmzname" {} #Matches to TF_VAR_vlan_dmzname
+variable "vlan_trustname" {} #Matches to TF_VAR_vlan_trustname
+variable "vlan_utilityname" {} #Matches to TF_VAR_vlan_utilityname
